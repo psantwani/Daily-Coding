60df8fdea9d1857082ccdff1a36419a3e48e47eb
Piyush Santwani <psantwani@vmware.com>
2019-11-05 03:53:27 +0530

    Initial commit

---
+++ src/com/piyush/easy/DCP1.java
@@ -0,0 +1,45 @@
+package com.piyush.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Daily Coding Problem: Problem #1 [Easy]
+ *
+ * This problem was recently asked by Google.
+ *
+ * Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+ *
+ * For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+ *
+ * Bonus: Can you do this in one pass?
+ */
+
+public class DCP1 {
+
+  public static void main(String[] args) {
+	Integer[] input = new Integer[]{10, 15, 3, 7};
+	Integer k = 17;
+    solution(input, k);
+
+	input = new Integer[]{10, 15, 3, 7};
+	k = 19;
+	solution(input, k);
+  }
+
+  private static boolean solution(Integer[] input, Integer k){
+	Map<Integer, Integer> map = new HashMap<>();
+	for(Integer i = 0; i < input.length; i++){
+	  Integer num = input[i];
+	  if(map.containsKey(num)){
+		System.out.println("True.");
+		return true;
+	  }
+	  map.put(k - num, i);
+	}
+
+	System.out.println("False.");
+	return false;
+  }
+
+}
