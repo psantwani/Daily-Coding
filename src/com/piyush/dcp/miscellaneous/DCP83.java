347321d8a75ead53a4c81b6bf18e53330557299b
Piyush Santwani <psantwani@vmware.com>
2019-11-14 10:58:03 +0530

    Invert a binary tree.

---
+++ src/com/piyush/dcp/miscellaneous/DCP83.java
@@ -0,0 +1,69 @@
+package com.piyush.dcp.miscellaneous;
+
+import com.piyush.dcp.google.DCP94;
+
+/**
+ * Invert a binary tree.
+ *
+ * For example, given the following tree:
+ *
+ *     a
+ *    / \
+ *   b   c
+ *  / \  /
+ * d   e f
+ * should become:
+ *
+ *   a
+ *  / \
+ *  c  b
+ *  \  / \
+ *   f e  d
+ */
+public class DCP83 {
+
+  static class Node {
+
+    char data;
+    Node left, right;
+
+    Node(char item) {
+      data = item;
+      left = right = null;
+    }
+  }
+
+  public static void solution(Node root){
+    postOrder(root);
+  }
+
+  private static Node postOrder(Node root){
+    if(root == null){
+      return null;
+    }
+    if(root.left == null && root.right == null){
+      return root;
+    }
+
+    Node left = postOrder(root.left);
+    Node right = postOrder(root.right);
+
+    root.left = right;
+    root.right = left;
+
+    return root;
+  }
+
+  public static void main(String[] args){
+    Node root = new Node('a');
+    root.left = new Node('b');
+    root.right = new Node('c');
+
+    root.left.left = new Node('d');
+    root.left.right = new Node('e');
+
+    root.right.left = new Node('f');
+    solution(root);
+  }
+
+}
