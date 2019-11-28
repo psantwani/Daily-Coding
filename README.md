063de73870c7c24704af23224a12bf3726f623e7
Piyush Santwani <psantwani@vmware.com>
2019-11-28 21:57:43 +0530

    Volatile keyword

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 **Topics**
 
 1. [Starting Threads](src/com/piyush/multithreading/StartingThreads.java)
+2. [Volatile Keyword](src/com/piyush/multithreading/Volatile.java)
--- src/com/piyush/multithreading/Volatile.java
+++ src/com/piyush/multithreading/Volatile.java
@@ -1,4 +1,46 @@
 package com.piyush.multithreading;
 
+import java.util.Scanner;
+
+class Processor extends Thread{
+
+  /**
+   * When Java runs a new thread, it caches/optimizes the data that is uses, because it assumes that
+   * no outside code will update the data. But in this case, we are updating running from the main thread.
+   * Hence, if we don't instruct Java to not cache, it is possible that it will never know when running becomes false
+   * and it will keep running in infinite loop. Volatile keyword ensures that.
+   */
+  private volatile boolean running = true;
+
+    @Override
+    public void run(){
+      while (running){
+        System.out.println("Hello");
+        try {
+          Thread.sleep(1000);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+      }
+    }
+
+    public void stopRunning(){
+      running = false;
+    }
+}
+
 public class Volatile {
+  public static void main(String ... args){
+    Processor t1 = new Processor();
+    t1.start();
+
+    /*
+      This will pause the main thread till you enter something.
+     */
+    System.out.println("Press any key to stop thread.");
+    Scanner in = new Scanner(System.in);
+    in.nextLine();
+
+    t1.stopRunning();
+  }
 }
