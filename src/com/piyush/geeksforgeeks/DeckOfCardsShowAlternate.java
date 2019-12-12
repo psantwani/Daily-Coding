175e07fe5bc390f2761aaca6bb38ddd5b5cb5481
Piyush Santwani <psantwani@vmware.com>
2019-12-12 07:37:39 +0530

    Add comments

--- src/com/piyush/geeksforgeeks/DeckOfCardsShowAlternate.java
+++ src/com/piyush/geeksforgeeks/DeckOfCardsShowAlternate.java
@@ -40,9 +40,9 @@ public class DeckOfCardsShowAlternate {
   }
 
   public static void main(String ... args){
-    solution(new int[]{1,3,5,7,11,13,17});
-    solution(new int[]{1,3,5,7});
-    solution(new int[]{1,3,5,7,11,13,17,19,21});
-    solution(new int[]{1,3,5,7,11,13,17,19,21,22});
+    solution(new int[]{1,3,5,7,11,13,17}); // Prints [1, 13, 3, 11, 5, 17, 7]
+    solution(new int[]{1,3,5,7}); // Prints [1, 5, 3, 7]
+    solution(new int[]{1,3,5,7,11,13,17,19,21}); // Prints [1, 21, 3, 13, 5, 19, 7, 17, 11]
+    solution(new int[]{1,3,5,7,11,13,17,19,21,22}); // Prints [1, 13, 3, 22, 5, 17, 7, 21, 11, 19]
   }
 }
