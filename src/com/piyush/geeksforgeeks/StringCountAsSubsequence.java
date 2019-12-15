8a8c026829a7180d31abf4c044f9a7782a0a97de
Piyush Santwani <psantwani@gmail.com>
2019-12-15 22:23:11 +0530

    Update time complexity

--- src/com/piyush/geeksforgeeks/StringCountAsSubsequence.java
+++ src/com/piyush/geeksforgeeks/StringCountAsSubsequence.java
@@ -1,6 +1,6 @@
 package com.piyush.geeksforgeeks;
 
-// Time complexity : O(n). Space complexity : O(1)
+// Time complexity : O(m*n). Space complexity : O(1)
 public class StringCountAsSubsequence {
 
   /**
